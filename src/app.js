c518b6688c1abc971d7ec0f30ccd958fe1548c89
Nitesh <imniteshjain@gmail.com>
2021-02-06 11:19:48 +0530

    first express commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 8000;
+
+app.get('/' , (req,res) => {
+    res.send('home page express');
+});
+
+app.get('/about', (req, res) => {
+    res.send('about page express');
+});
+
+app.listen(port);
